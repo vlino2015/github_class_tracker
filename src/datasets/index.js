e7913d1ca1c7a6ee667858726736b67e06d82e62
Veronica Lino <veronica@digitalcrafts.com>
2020-06-10 11:09:28 -0500

    Update index.js

--- src/datasets/index.js
+++ src/datasets/index.js
@@ -1,61 +1,9 @@
 export const DC_INSTRUCTORS = [
 	'azamsharp',
 	'vlino2015',
-	'pizzapanther',
-	'joeynguyen'
 ];
 
-export const HTX_NOV_2017_COHORT = [
-	'Jesus369',
-	'danielbdyer',
-	'williamtsanders',
-	'bradenschiller',
-	'BrandonB2686',
-	'baxter1707',
-	'swang2017',
-	'kelvin-mai',
-	'ninikat',
-	'noueilaty'
-];
 
-export const HTX_FEB_2018_COHORT = [
-	'carrda',
-	'kristenbaldwin',
-	'alani989',
-	'kmcc505',
-	'Muckler',
-	'Nhatseafood',
-	'andytanghr',
-	'thigabiga',
-	'TheBrotherFromASouthernMother',
-	'H0bbyist',
-	'AuriferousAurora',
-	'zzhang46516',
-	'NathanIzuora',
-	'JediJakeIsCOOL',
-	'dhull33',
-	'adrian-jablonski',
-	'CSchuster1995',
-	'callumbyers',
-	'Cristobal3'
-];
-
-export const HTX_FLEX_2018_COHORT = [
-	'Klgibson528',
-	'iuriarte',
-	'Zengeol',
-	'electricgnome',
-	'Julianpse',
-	'jessicapolansky',
-	'laddeyboy',
-	'jaggie84',
-	'scm2nycotx',
-	'aaronkyl',
-	'Salty-Potato',
-	'JimHe7',
-	'ziqingW',
-	'schmidtna'
-];
 
 export const HTX_IMMERSIVE_2020_COHORT = [
 	'darroyo97',
