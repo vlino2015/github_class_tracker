2ac238f2d7d6ae3899da64343954d45876c8372b
Veronica <veronica@digitalcrafts.com>
2020-06-10 10:39:00 -0500

    june 2020 immersive class

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import DC from '../assets/digitalcrafts.png';
+import User from './User';
+import { HTX_IMMERSIVE_JUNE_2020_COHORT as cohort } from '../datasets';
+
+export default class App extends Component {
+	render() {
+		const list = cohort
+			.sort((a, b) => {
+				if (a.toLowerCase() < b.toLowerCase()) return -1;
+				if (a.toLowerCase() > b.toLowerCase()) return 1;
+				return 0;
+			})
+			.map((name, i) => <User name={name} key={i} />);
+
+		return (
+			<div className="App">
+				<h1 style={{ textAlign: 'center' }}>
+					<img src={DC} alt="DigitalCrafts logo" style={{ width: '5rem' }} />
+					Current Cohort
+				</h1>
+				<ul>{list}</ul>
+			</div>
+		);
+	}
+}
---
+++ src/datasets/index.js
@@ -0,0 +1,94 @@
+export const DC_INSTRUCTORS = [
+	'azamsharp',
+	'vlino2015',
+	'pizzapanther',
+	'joeynguyen'
+];
+
+export const HTX_NOV_2017_COHORT = [
+	'Jesus369',
+	'danielbdyer',
+	'williamtsanders',
+	'bradenschiller',
+	'BrandonB2686',
+	'baxter1707',
+	'swang2017',
+	'kelvin-mai',
+	'ninikat',
+	'noueilaty'
+];
+
+export const HTX_FEB_2018_COHORT = [
+	'carrda',
+	'kristenbaldwin',
+	'alani989',
+	'kmcc505',
+	'Muckler',
+	'Nhatseafood',
+	'andytanghr',
+	'thigabiga',
+	'TheBrotherFromASouthernMother',
+	'H0bbyist',
+	'AuriferousAurora',
+	'zzhang46516',
+	'NathanIzuora',
+	'JediJakeIsCOOL',
+	'dhull33',
+	'adrian-jablonski',
+	'CSchuster1995',
+	'callumbyers',
+	'Cristobal3'
+];
+
+export const HTX_FLEX_2018_COHORT = [
+	'Klgibson528',
+	'iuriarte',
+	'Zengeol',
+	'electricgnome',
+	'Julianpse',
+	'jessicapolansky',
+	'laddeyboy',
+	'jaggie84',
+	'scm2nycotx',
+	'aaronkyl',
+	'Salty-Potato',
+	'JimHe7',
+	'ziqingW',
+	'schmidtna'
+];
+
+export const HTX_IMMERSIVE_2020_COHORT = [
+	'darroyo97',
+	'meryemkmr',
+	'abdenny',
+	'daniel1dolan',
+	'efishent',
+	'jaye-j',
+	'sjetho',
+	'JohnKearney2020',
+	'samiylo',
+	'JesseMart',
+	'driver1213',
+	'matthewtryan99',
+	'Foorkan14',
+	'Kazim786',
+	'getrealzx',
+	'PageSeanA',
+	'AlinaNBelova',
+	'Nicholas-Nwanochie'
+
+];
+
+export const HTX_IMMERSIVE_JUNE_2020_COHORT = [
+	'dgelok',
+	'cmbbarbo',
+	'woody-connell',
+	'CM-David',
+	'joevarghese',
+	'petersonprojects',
+	'JeremyRoussel',
+	'cortezmb',
+	'RjEppenger'
+]
+
+//Diego, Nicholas
